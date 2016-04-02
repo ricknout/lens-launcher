f672f29ed5102fe60d2273c3927189e7e5ed917a
Nicholas Rout <nicholasrout@gmail.com>
2016-04-02 20:39:46 +0200

    Settings & UI Update
    Added launch vibrate setting, updated app icon / task description

--- LensLauncher/app/src/main/java/nickrout/lenslauncher/ui/SettingsActivity.java
+++ LensLauncher/app/src/main/java/nickrout/lenslauncher/ui/SettingsActivity.java
@@ -1,6 +1,11 @@
 package nickrout.lenslauncher.ui;
 
+import android.app.ActivityManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import nickrout.lenslauncher.R;
@@ -14,5 +19,17 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setTaskDescription();
+    }
+
+    private void setTaskDescription() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Bitmap appIconBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            ActivityManager.TaskDescription taskDescription = new ActivityManager.TaskDescription(
+                    getString(R.string.app_name),
+                    appIconBitmap,
+                    ContextCompat.getColor(getBaseContext(), R.color.colorPrimaryDark));
+            setTaskDescription(taskDescription);
+        }
     }
 }
