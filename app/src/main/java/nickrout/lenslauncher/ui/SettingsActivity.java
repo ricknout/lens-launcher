0c9969d213bbd5efac04492932bd13b6d97bacc0
Nicholas Rout <nicholasrout@gmail.com>
2016-05-23 23:24:46 +0200

    Bump version for release

--- app/src/main/java/nickrout/lenslauncher/ui/SettingsActivity.java
+++ app/src/main/java/nickrout/lenslauncher/ui/SettingsActivity.java
@@ -2,7 +2,6 @@ package nickrout.lenslauncher.ui;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
