ae1248c48c5f4cd770bac9d6de81d9b9d65e3ed2
CreaRo <bhardwaj.rish@gmail.com>
2016-06-05 01:04:29 +0530

    Add LauncherUtil class, menu item for alter launcher

--- app/src/main/java/nickrout/lenslauncher/ui/FakeLauncherActivity.java
+++ app/src/main/java/nickrout/lenslauncher/ui/FakeLauncherActivity.java
@@ -1,57 +1,14 @@
 package nickrout.lenslauncher.ui;
 
-import android.app.Application;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import nickrout.lenslauncher.R;
 
 public class FakeLauncherActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_launcher);
-    }
-
-    public static boolean isMyAppLauncherDefault(Application application) {
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = application.getPackageManager().resolveActivity(intent, 0);
-        if (res.activityInfo == null) {
-            return false;
-        } else if ("android".equals(res.activityInfo.packageName)) {
-            return false;
-        } else {
-            if (res.activityInfo.packageName.equals(application.getPackageName())) return true;
-            else
-                return false;
-        }
-    }
-
-    public static void resetPreferredLauncherAndOpenChooser(Context context) {
-        PackageManager packageManager = context.getPackageManager();
-        ComponentName componentName = new ComponentName(context, nickrout.lenslauncher.ui.FakeLauncherActivity.class);
-
-        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-
-        Intent selector = new Intent(Intent.ACTION_MAIN);
-        selector.addCategory(Intent.CATEGORY_HOME);
-        selector.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(selector);
-
-        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
-    }
-
-    public static void launchAppChooser(Context context) {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
     }
 }
