38faa2cbd777130197f68875067e961c485fc041
Nick Rout <nick@bitx.co>
2016-06-06 16:28:56 +0200

    Collapsing toolbar layout in About screen

--- app/src/main/java/nickrout/lenslauncher/ui/AboutActivity.java
+++ app/src/main/java/nickrout/lenslauncher/ui/AboutActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
@@ -26,14 +27,18 @@ public class AboutActivity extends BaseActivity {
     @Bind(R.id.text_view_about)
     TextView mTextViewAbout;
 
-    @Bind(R.id.image_about)
+    @Bind(R.id.backdrop)
     ImageView mImageAbout;
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setupText();
         mImageAbout.post(new Runnable() {
